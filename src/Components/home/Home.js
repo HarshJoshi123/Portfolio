237341f18052bd198dbf2f9b0f0be50e2d569ca8
HarshJoshi123 <harsh2606joshi@gmail.com>
2021-12-21 17:20:35 +0530

    resume changed

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -24,7 +24,7 @@ function Home() {
                             </h3>
                         </div>
                         <div className="home-buttons ml-2 d-flex justify-content-center">
-                            <a href="https://drive.google.com/file/d/1CA_HVfw_gkp3kP9GHL-gwoq21iS0svFT/view?usp=sharing" className="btn primary-btn" rel="noreferrer" target="_blank">
+                            <a href="https://drive.google.com/file/d/1Mlg_AYP96RDGbm4n_Vw7Ijy0l4pXgqS8/view?usp=sharing" className="btn primary-btn" rel="noreferrer" target="_blank">
                                 <button className="btn text-white resume-btn blue ">Get Resume </button>
                             </a>
                         </div>
