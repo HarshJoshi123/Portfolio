058391cc63ee0f3197b407d71811169cc99d667a
HarshJoshi123 <harsh2606joshi@gmail.com>
2022-01-02 12:01:04 +0530

    font size

--- src/Components/experience/index.js
+++ src/Components/experience/index.js
@@ -18,7 +18,7 @@ const Experience = () => {
 
                     <Step active={true} key={1}>
                         <StepLabel  >
-                            <h3 style={{ color: 'white' }}>ANTARES TECH</h3>
+                            <div style={{ color: 'white' }}><h3>Software Developer  </h3><span style={{marginLeft:'20px'}}>Antares Tech</span></div>
                         </StepLabel>
                         <StepContent>
                             <Grid container >
@@ -31,7 +31,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>1.) End to End development of MERN stack projects called  BOSS(Business
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>1.) End to End development of MERN stack projects called  BOSS(Business
                                             Objective Scoring System) which generates agricultural reports based on given factors and a podcast application which hosts 75000 podcasts till date.
                                         </span>} />
                                     </ListItem>
@@ -43,7 +43,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>2.) Resolved several bugs in these applications making them failure proof. </span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>2.) Resolved several bugs in these applications making them failure proof. </span>} />
                                     </ListItem>
                                     <ListItem
                                         //className={classes.listItem}
@@ -53,7 +53,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>3.) Improvement in the speed of products by 30% by optimizing the code </span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>3.) Improvement in the speed of products by 30% by optimizing the code </span>} />
                                     </ListItem>
                                 </List>
 
@@ -63,7 +63,7 @@ const Experience = () => {
                     </Step>
                     <Step key={2} active={true}>
                         <StepLabel >
-                            <h3 style={{ color: 'white' }}>NAMASYS CONSULTANTS LLP</h3>
+                            <div style={{ color: 'white' }}><h3>Web Developer Intern </h3> <span style={{marginLeft:'20px'}}>Namasys Consultants LLP</span></div>
                         </StepLabel>
                         <StepContent>
                             <Grid container>
@@ -73,7 +73,7 @@ const Experience = () => {
                                         role={undefined}
 
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>1.) Worked on SACRD project which is a robotic process automation solution for multi-currency,multi-GAAP financial reporting automation and business intelligence. </span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>1.) Worked on SACRD project which is a robotic process automation solution for multi-currency,multi-GAAP financial reporting automation and business intelligence. </span>} />
                                     </ListItem>
                                     <ListItem
                                         //className={classes.listItem}
@@ -83,7 +83,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>2.) Built several key components using react.js and integrated api's with redux.</span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>2.) Built several key components using react.js and integrated api's with redux.</span>} />
                                     </ListItem>
                                     <ListItem
                                         //className={classes.listItem}
@@ -93,7 +93,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>3.) Worked on material-ui framework for building modern designs.</span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>3.) Worked on material-ui framework for building modern designs.</span>} />
                                     </ListItem>
                                 </List>
 
@@ -102,7 +102,7 @@ const Experience = () => {
                     </Step>
                     <Step key={3} active={true}>
                         <StepLabel >
-                            <h3 style={{ color: 'white' }}>TRICERIA SOLUTIONS</h3>
+                            <div style={{ color: 'white' }}><h3>Web Developer Intern </h3><span style={{marginLeft:'20px'}}>Triceria Solutions</span></div>
                         </StepLabel>
                         <StepContent>
                             <Grid container>
@@ -116,7 +116,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText style={{ fontSize: '20px !important' }} primary={<span style={{ fontSize: '20px' }}>1.) Developed a recruitment web application using React.js and Redux. </span>} />
+                                        <ListItemText  primary={<span style={{ fontSize: '18px' }}>1.) Developed a recruitment web application using React.js and Redux. </span>} />
                                     </ListItem>
                                     <ListItem
                                         //className={classes.listItem}
@@ -126,7 +126,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>2.) Communicated and worked closely with clients to ensure that their requirements were fully met. </span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>2.) Communicated and worked closely with clients to ensure that their requirements were fully met. </span>} />
                                     </ListItem>
                                     <ListItem
                                         //className={classes.listItem}
@@ -136,7 +136,7 @@ const Experience = () => {
                                         role={undefined}
                                     //onClick={() => this.onChecked(id)}
                                     >
-                                        <ListItemText primary={<span style={{ fontSize: '20px' }}>3.) Developed website using AGILE methodology. </span>} />
+                                        <ListItemText primary={<span style={{ fontSize: '18px' }}>3.) Developed website using AGILE methodology. </span>} />
                                     </ListItem>
                                 </List>
                             </Grid>
